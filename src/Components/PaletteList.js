3fd5f35d30b4e5f35918c9a82ab95bb33047c0a5
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-12 00:30:43 +0100

    Start syling mini palette

--- src/Components/PaletteList.js
+++ src/Components/PaletteList.js
@@ -7,13 +7,12 @@ class PaletteList extends Component {
     const { palettes } = this.props;
     return (
       <div>
-        <MiniPalette />
         <h1>React Colors</h1>
         {palettes.map(palette => (
-          <p>
-            {' '}
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName} </Link>
-          </p>
+          <MiniPalette {...palette} />
+          // <p>
+          //   <Link to={`/palette/${palette.id}`}>{palette.paletteName} </Link>
+          // </p>
         ))}
       </div>
     );
