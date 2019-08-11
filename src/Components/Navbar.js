458f621146cb783f0ba9f0a152ca54dcec23db04
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-11 17:11:40 +0100

    Add snackbar and styled select

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
@@ -9,16 +12,24 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     // control input
-    this.state = { format: 'hex' };
-    this.handleChange = this.handleChange.bind(this);
+    // {
+    // open = Snackbar @ bottm left,
+    // format = rgba || hex || rgba
+    // }
+    this.state = { format: 'hex', open: false };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
   // controlled input
-  handleChange(e) {
-    this.setState({ format: e.target.value });
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true });
     // example of async nature of setState line below may not pass new state
     // this.props.handleChange(this.state.format);
     // either pass callback to setState or use below
     this.props.handleChange(e.target.value);
+  }
+  closeSnackbar() {
+    this.setState({ open: false });
   }
   // state = {  }
 
@@ -37,12 +48,27 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select value={format} onChange={this.handleChange}>
+          <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">Hex - #ffffff</MenuItem>
             <MenuItem value="rgb">rgb - rgb(255,255,255) </MenuItem>
             <MenuItem value="rgba">rgba - rgba(255,255,255, 1.0) </MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format changed to {format.toUpperCase()}! </span>}
+          contentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton onClick={this.closeSnackbar} colors="inherit" keys="close" aria-label="close">
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
