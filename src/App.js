9ef1207589223afc0056b8afa10f0283186fa908
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-11 01:04:41 +0100

    Added ColorBox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import Palette from './Palette';
+import Palette from './Components/Palette';
 import seedColors from './seedColors';
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div className="palette">
-        <div className="Palette-colors" />
-
-        {/* <Palette {...seedColors[4]} /> */}
+      <div className="App">
+        <Palette {...seedColors[4]} />
       </div>
     );
   }
 }
-
 export default App;
