0d682596992e9d51e1eba50ef4db50d251562ffe
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-11 04:18:41 +0100

    Add style  color slider, STYLES NOT WORKING READ SORCE SLIDER RCs

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import './Palette.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Palette.css';
+
 class Palette extends Component {
   constructor(props) {
     super(props);
@@ -22,7 +23,10 @@ class Palette extends Component {
 
     return (
       <div className="Palette">
-        <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+        <div className="slider">
+          <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+        </div>
+
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
