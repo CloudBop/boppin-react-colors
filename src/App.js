ca8d97aeda64cf2d89611fdcd1fa016aae004771
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-11 03:21:20 +0100

    Add Chroma to derive colors from hex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Palette from './Components/Palette';
+import { generatePalette } from './helpers/colorChromaHelpers';
 import seedColors from './seedColors';
 
 class App extends Component {
   state = {};
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div className="App">
         <Palette {...seedColors[4]} />
