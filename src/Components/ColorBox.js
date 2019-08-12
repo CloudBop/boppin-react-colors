a6887036328cf533c2cefc6eb55c1cdbf5df0c6b
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-12 18:44:15 +0100

    Adjust color of text to contrast using chroma contrast

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import chroma from 'chroma-js';
 class ColorBox extends Component {
   constructor(props) {
     super(props);
@@ -17,7 +18,8 @@ class ColorBox extends Component {
   render() {
     const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
-
+    const onDarkContrast = chroma.contrast(background, 'black') < 6;
+    const onLightContrast = chroma.contrast(background, 'black') >= 6;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
@@ -26,19 +28,19 @@ class ColorBox extends Component {
           {/* overlay finished animation */}
           <div className={`copy-message ${copied && 'show'}`}>
             <h1>Copied!</h1>
-            <p> {background}</p>
+            <p className={`${onLightContrast && 'dark-text'}`}> {background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span> {name}</span>
+              <span className={`${onDarkContrast && 'light-text'}`}> {name}</span>
             </div>
-            <button className="copy-button">Copy</button>
+            <button className={`copy-button  ${onLightContrast && 'dark-text'}`}>Copy</button>
             {/* url as prop from parent */}
             {/* <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}> */}
             {/* show MORE link on colorbox */}
             {showLink && (
               <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                <span className="see-more">More</span>
+                <span className={`see-more ${onLightContrast && 'dark-text'}`}> MORE </span>
               </Link>
             )}
           </div>
