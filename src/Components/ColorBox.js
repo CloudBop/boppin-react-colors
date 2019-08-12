75db649c5762176d7d76d826ddcf4456aa1dccc9
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-12 22:47:38 +0100

    Finished Palette refactoring CSS ColorBox using JSS

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -25,6 +25,17 @@ const styles = {
   colorName: {
     color: props => (chroma.contrast(props.background, 'black') < 6 ? 'white' : 'rgba(0,0,0,0.6)')
   },
+  boxContent: {
+    width: '100%',
+    left: '0',
+    bottom: '0',
+    position: 'absolute',
+    padding: '10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px'
+  },
   seeMore: {
     background: 'rgba(255, 255, 255, 0.3)',
     position: 'absolute',
@@ -56,6 +67,56 @@ const styles = {
     textTransform: 'uppercase',
     border: 'none',
     opacity: '0'
+  },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.5s ease-in-out',
+    transform: 'scale(0.1)'
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute'
+  },
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    color: 'white',
+    opacity: '0',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase'
+    },
+    '& p': {
+      fontSize: '2rem',
+      fontWeight: '100'
+    }
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.3s'
   }
 };
 
@@ -78,14 +139,14 @@ class ColorBox extends Component {
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className={classes.ColorBox}>
           {/* overlay to cover whole screen on copy */}
-          <div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
+          <div style={{ background }} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
           {/* overlay finished animation */}
-          <div className={`copy-message ${copied && 'show'}`}>
+          <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
             <h1>Copied!</h1>
             <p className={classes.copyText}> {background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}> {name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
