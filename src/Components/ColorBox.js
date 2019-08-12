0dc9bc7158a0b450edff201e487a47da387b965d
Colin Rowntree <colinrowntree@Colins-MacBook-Pro.local>
2019-08-12 22:14:32 +0100

    Start refactoring CSS ColorBox using JSS

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -3,6 +3,62 @@ import { Link } from 'react-router-dom';
 import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import chroma from 'chroma-js';
+import { withStyles } from '@material-ui/styles';
+
+// withStyles wrpped into HOC
+const styles = {
+  ColorBox: {
+    width: '20%',
+    height: props => (props.showingFullPalette ? '25%' : '50%'),
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    '&:hover button': {
+      opacity: '1'
+    }
+  },
+  copyText: {
+    color: props => (chroma.contrast(props.background, 'black') < 6 ? 'rgba(0,0,0,0.6)' : 'white')
+  },
+  colorName: {
+    color: props => (chroma.contrast(props.background, 'black') < 6 ? 'white' : 'rgba(0,0,0,0.6)')
+  },
+  seeMore: {
+    background: 'rgba(255, 255, 255, 0.3)',
+    position: 'absolute',
+    border: 'none',
+    right: '0',
+    bottom: '0',
+    color: props => (chroma.contrast(props.background, 'black') < 6 ? 'white' : 'rgba(0,0,0,0.6)'),
+    width: '60px',
+    height: '30px',
+    textAlign: 'center',
+    lineHeight: '30px',
+    textTransform: 'uppercase'
+  },
+  copyButton: {
+    width: '100px',
+    height: '30px',
+    position: 'absolute',
+    display: 'inline-block',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    textAlign: 'center',
+    outline: 'none',
+    background: 'rgba(255, 255, 255, 0.3)',
+    fontSize: '1rem',
+    lineHeight: '30px',
+    color: props => (chroma.contrast(props.background, 'black') < 6 ? 'white' : 'rgba(0,0,0,0.6)'),
+    textTransform: 'uppercase',
+    border: 'none',
+    opacity: '0'
+  }
+};
+
 class ColorBox extends Component {
   constructor(props) {
     super(props);
@@ -16,31 +72,29 @@ class ColorBox extends Component {
     });
   }
   render() {
-    const { name, background, moreUrl, showLink } = this.props;
+    const { name, background, moreUrl, showingFullPalette, classes } = this.props;
     const { copied } = this.state;
-    const onDarkContrast = chroma.contrast(background, 'black') < 6;
-    const onLightContrast = chroma.contrast(background, 'black') >= 6;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className="ColorBox">
+        <div style={{ background }} className={classes.ColorBox}>
           {/* overlay to cover whole screen on copy */}
           <div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
           {/* overlay finished animation */}
           <div className={`copy-message ${copied && 'show'}`}>
             <h1>Copied!</h1>
-            <p className={`${onLightContrast && 'dark-text'}`}> {background}</p>
+            <p className={classes.copyText}> {background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span className={`${onDarkContrast && 'light-text'}`}> {name}</span>
+              <span className={classes.colorName}> {name}</span>
             </div>
-            <button className={`copy-button  ${onLightContrast && 'dark-text'}`}>Copy</button>
+            <button className={classes.copyButton}>Copy</button>
             {/* url as prop from parent */}
             {/* <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}> */}
             {/* show MORE link on colorbox */}
-            {showLink && (
+            {showingFullPalette && (
               <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                <span className={`see-more ${onLightContrast && 'dark-text'}`}> MORE </span>
+                <span className={classes.seeMore}> MORE </span>
               </Link>
             )}
           </div>
@@ -50,4 +104,4 @@ class ColorBox extends Component {
   }
 }
 
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
